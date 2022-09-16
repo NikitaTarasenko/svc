5b17dfb8bc2597a5e19ba3a2457f14afcc9acbef
NikitaTarasenko <36858449+NikitaTarasenko@users.noreply.github.com>
2022-09-16 20:41:07 +0300

    csstransition

--- src/layouts/sections/DealFlow.js
+++ src/layouts/sections/DealFlow.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState, useTransition } from "react";
+import React, { useEffect, useState } from "react";
 import SectionGrey from "./SectionGrey";
+import { CSSTransition } from "react-transition-group";
 
 const DealFlow = () => {
   const [hoverItem, setHoverItem] = useState(0);
+  const [anime, setAnime] = useState(false);
   const [prevHoverItem, setPrevHoverItem] = useState(0);
   const [firsth0verDone, setFirsth0verDone] = useState(false);
   const [step, setStep] = useState([
@@ -23,23 +25,26 @@ const DealFlow = () => {
       text: `We select the best and organize online meeting with founders and club memebers to decide on investing.`,
     },
   ]);
-//   const transition = useTransition({})
-
-
-
-
-
-
+  const [currentStep, setCurrentStep] = useState(step[0])
+ 
 
   useEffect(() => {
     const hoverBG = document.querySelector(".dealFlow__left__item__hover");
     if (firsth0verDone) {
       hoverBG.classList.remove(`dealFlow__left__item__hover_${prevHoverItem}`);
     }
+
     hoverBG.classList.add(`dealFlow__left__item__hover_${hoverItem}`);
-   
+    const timeoutStepCount = setTimeout(()=>{
+      setCurrentStep(step[hoverItem]);
+    },500)
     setPrevHoverItem(hoverItem);
     setFirsth0verDone(true);
+    setAnime(false);
+
+    return()=>{
+      clearTimeout(timeoutStepCount);
+    }
   }, [hoverItem]);
 
   return (
@@ -49,17 +54,35 @@ const DealFlow = () => {
 
       <div className="dealFlow">
         <div className="dealFlow__left">
-          <div className="dealFlow__left__item" onMouseEnter={() => setHoverItem(0)}>
+          <div
+            className="dealFlow__left__item"
+            onMouseEnter={() => {
+              setHoverItem(0);
+              setAnime(true);
+            }}
+          >
             <div className="dealFlow__left__count">1,500</div>
             <div className="dealFlow__left__descr">startup applications</div>
           </div>
 
-          <div className="dealFlow__left__item" onMouseEnter={() => setHoverItem(1)}>
+          <div
+            className="dealFlow__left__item"
+            onMouseEnter={() => {
+              setHoverItem(1);
+              setAnime(true);
+            }}
+          >
             <div className="dealFlow__left__count">200</div>
             <div className="dealFlow__left__descr">pre-selected deals</div>
           </div>
 
-          <div className="dealFlow__left__item" onMouseEnter={() => setHoverItem(2)}>
+          <div
+            className="dealFlow__left__item"
+            onMouseEnter={() => {
+              setHoverItem(2);
+              setAnime(true);
+            }}
+          >
             <div className="dealFlow__left__count">3-4</div>
             <div className="dealFlow__left__descr">invested deals</div>
           </div>
@@ -67,11 +90,17 @@ const DealFlow = () => {
           <div className="dealFlow__left__item__hover"></div>
         </div>
         <div className="dealFlow__right">
-          {/* <CSSTransition> */}
-          <div className="dealFlow__right__count">Step {step[hoverItem].id + 1}</div>
-          <div className="dealFlow__right__title">{step[hoverItem].title}</div>
-          <div className="dealFlow__right__text" dangerouslySetInnerHTML={{__html:step[hoverItem].text}}></div>
-          {/* </CSSTransition> */}
+          <>
+            {/* <CSSTransition in={anime} classNames="dealFlowWhite" timeout={400}> */}
+              <div className="dealFlow__right__count"> Step {currentStep.id + 1} </div>
+            {/* </CSSTransition> */}
+            <CSSTransition in={anime} classNames="dealFlowWhite" timeout={600}>
+              <div className="dealFlow__right__title">{currentStep.title}</div>
+            </CSSTransition>
+            <CSSTransition in={anime} classNames="dealFlowWhite" timeout={600}>
+              <div className="dealFlow__right__text" dangerouslySetInnerHTML={{ __html: currentStep.text }}></div>
+            </CSSTransition>
+          </>
         </div>
       </div>
     </SectionGrey>
