b14baad026c7f4339f3eea8cf9898e9a15830639
NikitaTarasenko <36858449+NikitaTarasenko@users.noreply.github.com>
2022-12-28 17:04:08 +0200

    changed DealFlow

---
+++ src/layouts/svgs/DFlow2.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const DFlow2 = () => {
+  return (
+    <div>
+      <svg width="96" height="96" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M45.2993 59.7591C43.3618 63.0222 38.6382 63.0222 36.7007 59.7591L7.48442 10.5527C5.50548 7.21973 7.9075 3 11.7837 3L70.2163 3C74.0925 3 76.4945 7.21974 74.5156 10.5527L45.2993 59.7591Z"
+          fill="url(#paint0_linear_1388_1347)"
+          fillOpacity="0.5"
+        />
+        <path
+          d="M38.4087 73.4423C39.5663 71.4578 42.4337 71.4578 43.5913 73.4423L52.3682 88.4884C53.5349 90.4884 52.0923 93 49.7769 93H32.2231C29.9077 93 28.4651 90.4884 29.6318 88.4884L38.4087 73.4423Z"
+          fill="white"
+          fillOpacity="0.2"
+        />
+        <path opacity="0.3" d="M15 67H67" stroke="white" strokeWidth="2" strokeLinecap="round" />
+        <path
+          d="M45.3046 59.7152C43.3693 62.9904 38.6307 62.9904 36.6954 59.7152L19.4576 30.5437C17.4881 27.2106 19.8907 23 23.7622 23L58.2378 23C62.1093 23 64.5119 27.2106 62.5424 30.5436L45.3046 59.7152Z"
+          fill="white"
+          fillOpacity="0.6"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_1388_1347"
+            x1="41"
+            y1="66.5"
+            x2="41"
+            y2="-6.5"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0.187498" stopColor="white" />
+            <stop offset="0.62709" stopColor="white" stopOpacity="0.298418" />
+            <stop offset="0.924347" stopColor="white" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+};
+
+export default DFlow2;
