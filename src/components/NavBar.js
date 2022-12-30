8b69e4a188e665f30bfaf1a194640f5f7dc82e90
NikitaTarasenko <36858449+NikitaTarasenko@users.noreply.github.com>
2022-12-31 01:53:42 +0200

    header_shit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,45 +1,63 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation  } from "react-router-dom";
 import "../App.css";
 import { ABOUTUS_ROUTE, FAQ_ROUTE, MAINPAGE_ROUTE, MEMBERSHIP_ROUTE } from "../utils/consts";
 
 const NavBar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  useEffect(() => {
+  const initG = ()=>{
+    ScrollTrigger.refresh();
     gsap.registerPlugin(ScrollTrigger);
+    const height = document.body.scrollHeight;
+    const nav = document.querySelector(".nav");
+    console.log('----------------------------')
+    console.log(height)
+    console.log('----------------------------')
+    
+    
 
     ScrollTrigger.create({
-      trigger: ".main-menu",
-      start: "top -150",
-      end: 99999,
+      trigger: ".container",
+      start: "top",
+      markers: true,
+      end: 500,
+      invalidateOnRefresh: true,
       onUpdate: (self) => {
-        console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "isActive:", self.isActive);
+        
         let { direction, isActive } = self;
-        const featureBoxes = document.querySelectorAll(".main-menu");
-        if (direction == -1) {
-          featureBoxes[0].classList.add("is-sticky");
-          featureBoxes[0].classList.remove("is-hidden");
+       
+        if (direction === -1) {
+          nav.classList.add("nav_scrolled");
+          nav.classList.remove("nav_hidden");
         }
-        if (direction == 1) {
-          featureBoxes[0].classList.add("is-hidden");
-          featureBoxes[0].classList.remove("is-sticky");
+        if (direction === 1) {
+          nav.classList.add("nav_hidden");
+          nav.classList.remove("nav_scrolled");
         }
-        if (isActive == true) {
-          featureBoxes[0].classList.remove("is-at-top");
-        } else if (isActive == false) {
-          featureBoxes[0].classList.add("is-at-top");
-          featureBoxes[0].classList.remove("is-sticky");
+        if (isActive === true) {
+          nav.classList.remove("is-at-top");
+        } else if (isActive === false) {
+          nav.classList.add("is-at-top");
+          nav.classList.remove("nav_scrolled");
         }
       },
     });
+  }
+  useEffect(() => {
+    initG()
   }, []);
+
+  useEffect(()=>{
+    ScrollTrigger.refresh();
+  },[location.pathname])
 
   return (
     <div className="sectionWrap sectionWrap_noFlex">
-      <div className="nav nav_scrolled">
+      <div className="nav  nav_NoScrolled ">
         <div className="nav__logo" onClick={() => navigate(MAINPAGE_ROUTE)}>
           SiliconValleyClub
         </div>
